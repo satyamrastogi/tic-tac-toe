d5b5d00f3b782ddeec991b87b7e7a836145f6ad8
satyamrastogi <satyam.mnnit97@gmail.com>
2020-05-09 11:52:02 +0530

    Updates

---
+++ precache-manifest.cdd6b9c5e963a5c44a9e5716f0027ea7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cbd1fa34ddc90d4dcbaab28465e16a4",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "a224d979862d44e66c00",
+    "url": "/tic-tac-toe/static/css/main.a3b730ec.chunk.css"
+  },
+  {
+    "revision": "7c0d8118c8dae30b5921",
+    "url": "/tic-tac-toe/static/js/2.7c5b8dd2.chunk.js"
+  },
+  {
+    "revision": "a224d979862d44e66c00",
+    "url": "/tic-tac-toe/static/js/main.d75bf276.chunk.js"
+  },
+  {
+    "revision": "25d94bd95813189daa86",
+    "url": "/tic-tac-toe/static/js/runtime-main.00b9c82d.js"
+  }
+]);
